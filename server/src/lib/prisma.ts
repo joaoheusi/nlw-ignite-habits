4ca5c6528d0ade5c962b123a319b2c7dc515b806
Joao Marcelo Heusi <jmarcelo.heusi@gmail.com>
2023-01-17 23:42:47 -0300

    aula 2 back + web

---
+++ server/src/lib/prisma.ts
@@ -0,0 +1,4 @@
+import { PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient();
+
+// for logging {log:["query"]}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,17 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
-
-app.get("/habits", async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
 
 app.register(cors);
+app.register(appRoutes);
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("http server running!");
+    console.log("✨ HTTP Server running! 🚀");
   });
