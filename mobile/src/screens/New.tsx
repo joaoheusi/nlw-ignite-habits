e8fa6fbde29add4b6f3947fa20aa3d426823b333
Joao Marcelo Heusi <jmarcelo.heusi@gmail.com>
2023-01-21 17:44:35 -0300

    aula 4 mobile

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -4,12 +4,14 @@ import {
   ScrollView,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
 import { useState } from "react";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import { api } from "../lib/axios";
 
 const availableWeekDays = [
   "Domingo",
@@ -23,6 +25,7 @@ const availableWeekDays = [
 
 export function New() {
   const [weekDays, setWeekDays] = useState<number[]>([]);
+  const [title, setTitle] = useState("");
 
   function handleToggleWeekDay(weekDayIndex: number) {
     if (weekDays.includes(weekDayIndex)) {
@@ -31,6 +34,27 @@ export function New() {
       );
     } else {
       setWeekDays((prevState) => [...prevState, weekDayIndex]);
+    }
+  }
+
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert(
+          "Novo hábito",
+          "Digite o nome do hábito e selecione a recorrência do hábito"
+        );
+      }
+      await api.post("/habits", {
+        title,
+        weekDays,
+      });
+      setTitle("");
+      setWeekDays([]);
+      Alert.alert("Novo hábito", "Hábito criado com sucesso!");
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Ops", "Não foi possível criar o novo hábito");
     }
   }
 
@@ -51,6 +75,8 @@ export function New() {
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 focus:border-green-300 border-zinc-800"
           placeholder="Exercícios, dormir bem, etc.."
           placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
         ></TextInput>
         <Text className="text-white mt-4 mb-3 font-semibold text-base">
           Qual a recorrência?
@@ -65,7 +91,10 @@ export function New() {
           ></Checkbox>
         ))}
 
-        <TouchableOpacity className="bg-green-600 flex-row rounded-md mt-6 w-full h-14 items-center justify-center ">
+        <TouchableOpacity
+          className="bg-green-600 flex-row rounded-md mt-6 w-full h-14 items-center justify-center "
+          onPress={handleCreateNewHabit}
+        >
           <Feather name="check" size={20} color={colors.white} />
           <Text className="font-semibold text-base text-white ml-2">
             Confirmar
